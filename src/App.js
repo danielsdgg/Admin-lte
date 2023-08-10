9c122cc3ff1c59c509ef10a08f8b5f594788fa8d
daniel <gathigidg26@gmail.com>
2023-08-10 21:59:43 +0300

    admin-lte

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+      <NavBar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
